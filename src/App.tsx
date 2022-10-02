cbcd40645d5273287490b685bdaf68f2aa93828e
anthonychao <qoosuperman@gmail.com>
2022-10-02 14:00:56 +0800

    feat: add login and logout function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import './App.css'
+import "./App.css";
 
-import type { FC } from 'react'
-import { Link } from 'react-router-dom'
+import type { FC } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { apiCurrentUser } from "./api";
 
 const App: FC = () => {
+  const [loginString, setLoginString] = useState("正在確認中");
+
+  useEffect(() => {
+    apiCurrentUser()
+      // @ts-expect-error: 先不管這裡
+      .then((res) => setLoginString(res.data.user.name))
+      .catch((error) => setLoginString(error.message));
+  }, []);
+
   return (
     <div className="App">
       Homepage
       <nav>
-        <Link to="/sign_in">Sign In</Link> |{' '}
+        <h3>狀態： {loginString}</h3>
+        <Link to="/sign_in">Sign In</Link> |{" "}
         <Link to="/sign_out">Sign Out</Link>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,7 +1,31 @@
-import type { FC } from 'react'
+import type { FC, MouseEventHandler } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { apiUserSignIn } from "../api";
 
 const SignIn: FC = () => {
-  return <div>This is Sign in Component</div>
-}
+  const navigate = useNavigate();
+  const handleSignIn: MouseEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    // @ts-expect-error: 先不管這裡
+    const email = e.target.email.value;
+    // @ts-expect-error: 先不管這裡
+    const password = e.target.password.value;
 
-export default SignIn
+    apiUserSignIn({ user: { email, password } })
+      .then(() => navigate("/"))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <form method="POST" onSubmit={handleSignIn}>
+      <label htmlFor="email">信箱</label>
+      <input type="text" id="email" name="email" />
+      <label htmlFor="password">密碼</label>
+      <input type="text" id="password" name="password" />
+      <button>送出</button>
+    </form>
+  );
+};
+
+export default SignIn;
