ca524ecd8f5b50b608346f8b69efacaac16b3ec3
anthonychao <qoosuperman@gmail.com>
2023-08-09 20:20:02 +0800

    style: formatting

--- src/components/Admin/BackStageGuardian/BackStageGuardian.tsx
+++ src/components/Admin/BackStageGuardian/BackStageGuardian.tsx
@@ -5,7 +5,7 @@ import Loading from "../../Loading";
 import NotFound from "../../NotFound";
 import { BackStageGuardianProps } from "./BackStageGuardian.type";
 
-const BackStageGuardian: FC<BackStageGuardianProps> = ({children}) => {
+const BackStageGuardian: FC<BackStageGuardianProps> = ({ children }) => {
   const [canEnterBackStage, setCanEnterBackStage] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -19,9 +19,9 @@ const BackStageGuardian: FC<BackStageGuardianProps> = ({children}) => {
       .catch((_error) => setCanEnterBackStage(false))
       .finally(() => setIsLoading(false));
   }, []);
-  if(isLoading) return(<Loading />)
-  if(!canEnterBackStage) return(<NotFound />)
-  return (<>{children}</>)
-}
+  if (isLoading) return <Loading />;
+  if (!canEnterBackStage) return <NotFound />;
+  return <>{children}</>;
+};
 
 export default BackStageGuardian;
